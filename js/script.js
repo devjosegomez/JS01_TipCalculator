d75faad79afd25564e5a8023aa229c809f860a77
devjosegomez <devjosegomez@gmail.com>
2019-10-31 22:21:17 -0600

    Example using backtip formatter to output tip value

--- js/script.js
+++ js/script.js
@@ -6,6 +6,6 @@ button.innerHTML = "Calculate Tip!";
 
 button.addEventListener("click", function(){
     let tip = cost.value * 0.15;
-    let tipMessage = "<h1>You should tip $" + tip.toFixed(2) + " on $" + cost.value + "</h1>"; 
+    let tipMessage = `<h1>You should tip $${tip.toFixed(2)} on $${ cost.value} </h1>`;
     output.innerHTML = tipMessage;
 });
