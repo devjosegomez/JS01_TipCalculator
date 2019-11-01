aabed5f0bda79dd60357387e25c65a47592852d3
devjosegomez <devjosegomez@gmail.com>
2019-10-31 22:09:11 -0600

    .toFixed(2) limit how many decimals are going to be displayed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 //Restaurant Tip Calculator
 const button = document.querySelector("button");
 const output = document.querySelector(".output");
+const cost = document.querySelector("input");
 button.innerHTML = "Calculate Tip!";
 
 button.addEventListener("click", function(){
-    output.innerHTML = "output";
+    let tip = cost.value * 0.15;
+    let tipMessage = "<h1>You should tip $" + tip.toFixed(2) + " on $" + cost.value + "</h1>"; 
+    output.innerHTML = tipMessage;
 });
