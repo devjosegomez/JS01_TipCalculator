bfa95106483b9f836da35ab2d2099df710283a81
devjosegomez <devjosegomez@gmail.com>
2019-10-31 21:49:05 -0600

    Using querySelector, addEventListener()

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//Restaurant Tip Calculator
+const button = document.querySelector("button");
+const output = document.querySelector(".output");
+button.innerHTML = "Calculate Tip!";
+
+button.addEventListener("click", function(){
+    output.innerHTML = "output";
+});
